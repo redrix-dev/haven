2ebd3c692ef5a772c418283389e0c230afe5abae
redrix-dev <redrixdevelopment@icloud.com>
2026-02-24 13:09:45 -0500

    refactor: complete phase 10 preload split

---
+++ src/preload/desktop-bridge.js
@@ -0,0 +1,33 @@
+const { DESKTOP_IPC_KEYS } = require('../shared/ipc/keys');
+
+const exposeDesktopBridge = ({ contextBridge, ipcRenderer }) => {
+  contextBridge.exposeInMainWorld('desktop', {
+    getAppSettings: () => ipcRenderer.invoke(DESKTOP_IPC_KEYS.SETTINGS_GET),
+    setAutoUpdateEnabled: (enabled) =>
+      ipcRenderer.invoke(DESKTOP_IPC_KEYS.SETTINGS_SET_AUTO_UPDATE, { enabled }),
+    setNotificationAudioSettings: (payload) =>
+      ipcRenderer.invoke(DESKTOP_IPC_KEYS.SETTINGS_SET_NOTIFICATION_AUDIO, payload),
+    getUpdaterStatus: () => ipcRenderer.invoke(DESKTOP_IPC_KEYS.UPDATER_STATUS_GET),
+    checkForUpdates: () => ipcRenderer.invoke(DESKTOP_IPC_KEYS.UPDATER_CHECK_NOW),
+    saveFileFromUrl: (payload) => ipcRenderer.invoke(DESKTOP_IPC_KEYS.MEDIA_SAVE_FROM_URL, payload),
+    consumeNextProtocolUrl: () => ipcRenderer.invoke(DESKTOP_IPC_KEYS.PROTOCOL_URL_CONSUME_NEXT),
+    onProtocolUrl: (listener) => {
+      if (typeof listener !== 'function') {
+        return () => {};
+      }
+
+      const wrappedListener = (_event, url) => {
+        listener(url);
+      };
+
+      ipcRenderer.on(DESKTOP_IPC_KEYS.PROTOCOL_URL_EVENT, wrappedListener);
+      return () => {
+        ipcRenderer.removeListener(DESKTOP_IPC_KEYS.PROTOCOL_URL_EVENT, wrappedListener);
+      };
+    },
+  });
+};
+
+module.exports = {
+  exposeDesktopBridge,
+};
